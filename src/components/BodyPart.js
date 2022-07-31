49d81d0efed5bf34b61525eb0552eb1449466c4f
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-07-31 20:26:54 +0300

    adding styles to modify cards

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -8,14 +8,14 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
     alignItems="center"
     justifyContent="center"
     className="bodyPart-card"
-    sx={bodyPart === item ? { borderTop: '4px solid #FF2625', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
+    sx={bodyPart === item ? { borderTop: '4px solid #057575', background: '#013942', borderRadius: '40px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#013942', borderRadius: '40px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px', borderTop: '4px solid #057575' }}
     onClick={() => {
       setBodyPart(item);
       window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
     }}
   >
     <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
-    <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
+    <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#0edfff" textTransform="capitalize"> {item}</Typography>
   </Stack>
 );
 
