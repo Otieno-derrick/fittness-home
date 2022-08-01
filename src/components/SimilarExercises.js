156c58fc94066c279c3ca9cd670b43c75e83dee1
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-08-01 08:30:06 +0300

    styling exercises

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -7,13 +7,13 @@ import Loader from './Loader';
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
   <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
     <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px' }} fontWeight={700} color="#000" mb="33px">
-      Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Target Muscle</span> exercises
+      People <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Also Searches</span> for.
     </Typography>
     <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
       {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
     </Stack>
     <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px', mt: { lg: '100px', xs: '60px' } }} fontWeight={700} color="#000" mb="33px">
-      Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Equipment</span> exercises
+      You can like <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>acordding to</span> your such
     </Typography>
     <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
       {equipmentExercises.length !== 0 ? <HorizontalScrollbar data={equipmentExercises} /> : <Loader />}
